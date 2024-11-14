897dd15e841c06911e30d6e78e83e718a3a76303
colabufo <colabufo.joseph@gmail.com>
2024-11-14 12:10:28 -0500

    testing js

---
+++ javascript.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+    $("#alerts").click(function() {
+        $("#HomePage").setAttribute(hidden, "hidden")
+        $("#alerts").removeAttribute(hidden)
+    })
+  
+  })
