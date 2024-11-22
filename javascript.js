c8cb849934a99ce0231502afe5439633204946d5
colabufo <colabufo.joseph@gmail.com>
2024-11-21 23:40:37 -0500

    removed javascript

--- javascript.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-
-    $("#alerts").click(function() {
-        $("#HomePage").setAttribute(hidden, "hidden")
-        $("#alerts").removeAttribute(hidden)
-    })
-  
-})
