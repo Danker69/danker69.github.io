0202c194e76c8bc24d5c381f8b6ebee2d713f5ab
colabufo <colabufo.joseph@gmail.com>
2024-12-01 13:24:18 -0500

    button functionality

---
+++ js.js
@@ -0,0 +1,27 @@
+function goToHome() {
+    document.getElementById("AlertPage").style.display = "none";
+    document.getElementById("DataPage").style.display = "none";
+    document.getElementById("AccountPage").style.display = "none"
+    document.getElementById("SettingsPage").style.display = "none";
+    document.getElementById("HomePage").style.display = "block";
+}
+
+function goToAlert() {
+    document.getElementById("HomePage").style.display = "none";
+    document.getElementById("AlertPage").style.display = "block";
+}
+
+function goToData() {
+    document.getElementById("HomePage").style.display = "none";
+    document.getElementById("DataPage").style.display = "block";
+}
+
+function goToAccount() {
+    document.getElementById("HomePage").style.display = "none";
+    document.getElementById("AccountPage").style.display = "block";
+}
+
+function goToSettings() {
+    document.getElementById("HomePage").style.display = "none";
+    document.getElementById("SettingsPage").style.display = "block";
+}
